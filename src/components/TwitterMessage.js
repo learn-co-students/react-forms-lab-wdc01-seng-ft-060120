13c9c1d66b17b8be854bbe4d45ca1eae02cbfc77
mjester93 <m.jester.93@gmail.com>
2020-08-07 16:32:20 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,19 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {message: ''};
+  }
+
+  updateStateOfTweetLength = (event) => {
+    this.setState({message: event.target.value})
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onChange={this.updateStateOfTweetLength} value={this.state.message}/>
+        <p>Remaining characters: {this.props.maxChars - this.state.message.length}</p>
       </div>
     );
   }
